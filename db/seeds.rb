bea666a3a6b1183e6a5dfb6b017b7278b94f6704
Michelle <maknegana@berkeley.edu>
2016-11-28 01:18:50 -0800

    initialization devise works

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+DayWeather.create(date: 1001, temp: 65, sky: 'cloudy', precipitation: 2, wind: 10, humidity: 30, location: 'Berkeley')
