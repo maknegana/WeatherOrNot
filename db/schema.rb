1afd91c3c32d81fa36378f2bd9ef5ee9df51cc28
Michelle <maknegana@berkeley.edu>
2016-11-28 14:33:46 -0800

    added all controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128120612) do
+ActiveRecord::Schema.define(version: 20161128222935) do
 
   create_table "day_weathers", force: :cascade do |t|
     t.integer  "date"
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 20161128120612) do
     t.integer  "neck"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "what_to_brings", ["user_id"], name: "index_what_to_brings_on_user_id"
 
 end
