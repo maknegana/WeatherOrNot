65cafdd5a81ff08a663fe0602556074969cf991d
Adriana Babakanian <ababakanian@berkeley.edu>
2016-11-29 10:31:30 -0800

    hid empty suggestions boxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # @most_recent_day_id = DayWeather.all.order(:id).last
-    # @cdw = DayWeather.find_by_id(@most_recent_day_id)
 
     offset = rand(DayWeather.count)
     @cdw = DayWeather.offset(offset).first
@@ -16,31 +14,18 @@ class HomeController < ApplicationController
       end
     end
 
-    # @rel_wtb = WhatToBring.create(:weather => 'cloudy',
-    #                                :top => 1,
-    #                                :bottom => 1,
-    #                                :shoes => 1,
-    #                                :hands => 1,
-    #                                :head => 1,
-    #                                :eyes => 1,
-    #                                :ears => 1,
-    #                                :neck => 1,
-    #                                :bring => 1
-    #                                )
-
-
     # Lists corresponding to the columns in the what_to_bring model. Index into
     # these lists to render the appopriate suggestion for each column that
     # is stored in the current user's what_to_bring for the current weather condition.
     @tops=["shirt", "sweater", "jacket", "rainjacket", "parka"]
     @bottoms=["shorts", "jeans", "jeans", "jeans", "jeans"]
     @shoes=["sandals", "sneakers", "sneakers", "rainboots", "snowboots"]
-    @hands=["none", "none", "none", "none", "gloves"]
+    @hands=["", "", "", "", "gloves"]
     @heads=["baseball cap", "beanie", "beanie", "beanie", "beanie"]
-    @eyes=["sunglasses", "none", "none", "none", "none"]
-    @ears=["none", "none", "none", "none", "earmuffs"]
-    @necks=["none", "none", "scarf", "scarf", "scarf"]
-    @brings=["none", "none", "umbrella", "umbrella", "none"]
+    @eyes=["sunglasses", "", "", "", ""]
+    @ears=["", "", "", "", "earmuffs"]
+    @necks=["", "", "scarf", "scarf", "scarf"]
+    @brings=["", "", "umbrella", "umbrella", ""]
   end
 
   def calendar
